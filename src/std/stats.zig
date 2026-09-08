9177c6cfa5e123c6c78938e1fbfc3314578415d1
Chris Collazo <ccollazo@geosonics.com>
2026-09-08 11:31:06 -0400

    Added RunningStats struct translated from Nim std/stats, with tests. Can use internally to calculate stats.

--- src/std/stats.zig
+++ src/std/stats.zig
@@ -6,6 +6,7 @@ const table_std = @import("table.zig");
 // const pool = @import("pool.zig");
 const Ts = root.T;
 
+const math = std.math;
 const typeof = root.typeof;
 const memory = revo.memory;
 const Data = memory.Data;
@@ -16,6 +17,153 @@ const Component = std.Uri.Component;
 const Table = revo.table.Table;
 const testing = revo.lang.testing;
 const table_methods = table_std.Impl;
+
+
+const RunningStat = struct {                    // An accumulator for statistical data.
+    // amount of pushed data
+    n: usize = 0,
+    // self-explaining
+    min: f64 = 0.0,
+    max: f64 = 0.0,
+    sum: f64 = 0.0,
+    // statistical moments, mom1 is mean
+    mom1: f64 = 0.0,
+    mom2: f64 = 0.0,
+    mom3: f64 = 0.0,
+    mom4: f64 = 0.0,
+
+    fn pushEle(self: *RunningStat, x: f64) void {
+        // Pushes a value `x` for processing.
+        if (self.n == 0) {
+            self.min = x;
+            self.max = x;
+        } else {
+            if (self.min > x)
+                self.min = x;
+            if (self.max < x)
+                self.max = x;
+        }
+        self.n += 1;
+        // See Knuth TAOCP vol 2, 3rd edition, page 232
+        self.sum += x;
+        const n_float = @as(f64, @floatFromInt(self.n));
+        const delta = x - self.mom1;
+        const delta_n = delta / n_float;
+        const delta_n2 = delta_n * delta_n;
+        const term1 = delta * delta_n * (n_float - 1);
+        self.mom4 += term1 * delta_n2 * (n_float*n_float - 3*n_float + 3) + 6*delta_n2*self.mom2 - 4*delta_n*self.mom3;
+        self.mom3 += term1 * delta_n * (n_float - 2) - 3*delta_n*self.mom2;
+        self.mom2 += term1;
+        self.mom1 += delta_n;
+    }
+
+    fn pushData(self: *RunningStat, data: *std.ArrayList(f64)) void {
+        for (data.items) |value| {
+            self.pushEle(value);
+        }
+    }
+
+    fn mean(self: *RunningStat) f64 {
+        // Computes the current mean of `self`.
+        return self.mom1;
+    }
+
+    fn variance(self: *RunningStat) f64 {
+        // Computes the current population variance of `self`.
+        const n_float = @as(f64, @floatFromInt(self.n));
+        return self.mom2 / n_float;
+    }
+
+    fn varianceS(self: *RunningStat) f64 {
+        // Computes the current sample variance of `self`.
+        if (self.n > 1) {
+            return self.mom2 / @as(f64, @floatFromInt(self.n - 1));
+        } else {
+            return 0.0;
+        }
+    }
+
+    fn standardDeviation(self: *RunningStat) f64 {
+        // Computes the current population standard deviation of `self`.
+        return math.sqrt(self.variance());
+    }
+
+    fn standardDeviationS(self: *RunningStat) f64 {
+        // Computes the current sample standard deviation of `self`.
+        return math.sqrt(self.varianceS());
+    }
+
+    fn skewness(self: *RunningStat) f64 {
+        // Computes the current population skewness of `self`.
+        const n_float = @as(f64, @floatFromInt(self.n));
+        return math.sqrt(n_float) * self.mom3 / math.pow(f64, self.mom2, 1.5);
+    }
+
+    fn skewnessS(self: *RunningStat) f64 {
+        // Computes the current sample skewness of `self`.
+        const n_float = @as(f64, @floatFromInt(self.n));
+        const s2 = self.skewness();
+        return math.sqrt(n_float*(n_float-1))*s2 / (n_float-2);
+    }
+
+    fn kurtosis(self: *RunningStat) f64 {
+        // Computes the current population kurtosis of `self`.
+        const n_float = @as(f64, @floatFromInt(self.n));
+        return n_float * self.mom4 / (self.mom2 * self.mom2) - 3.0;
+    }
+
+    fn kurtosisS(self: *RunningStat) f64 {
+        // Computes the current sample kurtosis of `self`.
+        const n_float = @as(f64, @floatFromInt(self.n));
+        return (n_float-1) / ((n_float-2)*(n_float-3)) * ((n_float+1)*self.kurtosis() + 6);
+    }
+};
+
+test "RunningStat struct and methods" {
+    const a = std.testing.allocator;
+    const expect = std.testing.expect;
+
+    var list: std.ArrayList(f64) = .empty;
+    defer list.deinit(a);
+    try list.append(a, 1.0);
+    try list.append(a, 2.0);
+    try list.append(a, 1.0);
+    try list.append(a, 4.0);
+    try list.append(a, 1.0);
+    try list.append(a, 4.0);
+    try list.append(a, 1.0);
+    try list.append(a, 2.0);
+
+    var runningStat: RunningStat = .{};
+    runningStat.pushData(&list);
+    const tolerance = 0.00001;
+
+    try expect(runningStat.n == 8);
+    try std.testing.expectApproxEqAbs(runningStat.mean(), 2.0, tolerance);
+    try std.testing.expectApproxEqAbs(runningStat.variance(), 1.5, tolerance);
+    try std.testing.expectApproxEqAbs(runningStat.varianceS(), 1.714285714285715, tolerance);
+    try std.testing.expectApproxEqAbs(runningStat.skewness(), 0.8164965809277261, tolerance);
+    try std.testing.expectApproxEqAbs(runningStat.skewnessS(), 1.018350154434631, tolerance);
+    try std.testing.expectApproxEqAbs(runningStat.kurtosis(), -1.0, tolerance);
+    try std.testing.expectApproxEqAbs(runningStat.kurtosisS(), -0.7000000000000008, tolerance);
+}
+
+const Statistics = struct {
+    running: RunningStat,
+    variance: f64,
+    varianceS: f64,
+    skewness: f64,
+    skewnessS: f64,
+    kurtosis: f64,
+    kurtosisS: f64,
+};
+
+// const RunningRegress = struct { // An accumulator for regression calculations.
+//     n: usize,                   // amount of pushed data
+//     x_stats: RunningStat,       // stats for the first set of data
+//     y_stats: RunningStat,       // stats for the second set of data
+//     s_xy: f64,                  // accumulated data for combined xy
+// };
 
 pub const Impl = struct {
     /// > stats:frequencies() -> table<any>
@@ -156,9 +304,6 @@ test "stats methods" {
 // harmonic_mean(data, weights=None)
 // Harmonic mean of data.
 
-// median(data)
-// Median (middle value) of data.
-
 // median_low(data)
 // Low median of data.
 
@@ -167,9 +312,6 @@ test "stats methods" {
 
 // median_grouped(data, interval=1.0)
 // Median (50th percentile) of grouped data.
-
-// mode(data)
-// Single mode (most common value) of discrete or nominal data.
 
 // multimode(data)
 // List of modes (most common values) of discrete or nominal data.
